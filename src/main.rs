b605f6d1b500dbd88f4c0b263bc750fdedf2d704
Gary Lynam <lynamgaz@yahoo.ie>
2018-12-05 00:07:11 +0000

    Fixing tests

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ fn process_input(starting:i32, commands: &str) -> i32 {
     for command in commands.split(',') {
         let result: i32 = command.parse().unwrap();
         count += result;
-
     }
     println!("{}", count);
     return count;
@@ -19,7 +18,7 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(process_input(1, ""), 1);
+        assert_eq!(process_input(1, "+1,-2,+3,+1"), 4);
     }
 }
 
